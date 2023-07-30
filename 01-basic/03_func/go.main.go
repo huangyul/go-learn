47153131f0411ac9ed98d6a3c43227349bb8771f
huanghyul <1103221645@qq.com>
2023-07-30 21:58:43 +0800

    func

--- 01-basic/03_func/go.main.go
+++ 01-basic/03_func/go.main.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
-
-// defer
 func main() {
-	defer func() {
-		fmt.Println("第一个defer")
-	}()
-	defer func() {
-		fmt.Println("第二个defer")
-	}()
+	// 1. defer执行顺序
+	// 第二个先输出
+	//defer func() {
+	//	fmt.Println("第一个defer")
+	//}()
+	//defer func() {
+	//	fmt.Println("第二个defer")
+	//}()
+
+	// 3. 输出问题
+	// 输出2，因为永远是return前才执行
+	//i := 0
+	//defer func() {
+	//	fmt.Println(i)
+	//}()
+	//i = 2
+	// 输出0，因为先存下了i
+	//i := 0
+	//defer func(val int) {
+	//	println(i)
+	//}(i)
+	//i = 2
 }
