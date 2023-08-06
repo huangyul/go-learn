c15fb66234a039ca7dc4886316b4e68cba62c239
huanghyul <1103221645@qq.com>
2023-08-06 10:28:39 +0800

    路由

--- 02-gin_gorm/gin_basic/main.go
+++ 02-gin_gorm/gin_basic/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/hello", func(context *gin.Context) {
-		context.String(http.StatusOK, "hello, gin")
+
+	// 静态路由
+	r.GET("/hello", func(ctx *gin.Context) {
+		// 获取查询参的方法
+		ctx.String(http.StatusOK, "hello, gin")
 	})
+
+	// 参数路由
+	r.GET("/users/:name", func(ctx *gin.Context) {
+		// 获取参数的方法
+		p := ctx.Param("name")
+		ctx.String(http.StatusOK, fmt.Sprintf("参数路由：%s", p))
+	})
+
+	// 匹配路由
+	r.GET("/order/*.html", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "匹配路由")
+	})
+
 	err := r.Run(":8080")
 	if err != nil {
 		return
 	}
+
 }
